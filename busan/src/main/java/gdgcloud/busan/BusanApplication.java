5400d0906a8d89201f571a3ee68d1618414b31a9
gugbab2 <jkhoo1997@gmail.com>
2022-08-13 01:54:48 +0900

    add defaultController

--- busan/src/main/java/gdgcloud/busan/BusanApplication.java
+++ busan/src/main/java/gdgcloud/busan/BusanApplication.java
@@ -2,6 +2,8 @@ package gdgcloud.busan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BusanApplication {
@@ -10,4 +12,12 @@ public class BusanApplication {
 		SpringApplication.run(BusanApplication.class, args);
 	}
 
+}
+
+@RestController
+class HelloGDG {
+	@GetMapping("/")
+	public String defaultController(){
+		return "HELLO GDG-CLOUD_BUSAN!";
+	}
 }
